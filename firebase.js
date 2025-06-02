0b04c04950ec5ea420ac3f72843ceb52508d7559
nahum1938 <rodrigueznahum940@gmail.com>
2025-06-02 00:14:08 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
-// Firebase config here
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLCNqa0B387EVNnIQrazoXanW0Q43s7Y",
+  authDomain: "novacoretransit-7ee86.firebaseapp.com",
+  databaseURL: "https://novacoretransit-7ee86-default-rtdb.firebaseio.com",
+  projectId: "novacoretransit-7ee86",
+  storageBucket: "novacoretransit-7ee86.appspot.com",
+  messagingSenderId: "649834732880",
+  appId: "1:649834732880:web:bc8eae809d1161070ea055",
+  measurementId: "G-27WD7PSVQW"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
