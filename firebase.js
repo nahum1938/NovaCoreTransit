35a1da99d6f2c19c74fd40d1d2656f21d41a0ed6
nahum1938 <rodrigueznahum940@gmail.com>
2025-06-01 22:54:39 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,55 +1,12 @@
-
-console.log("🔥 firebase.js cargado correctamente");
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import {
-  getDatabase,
-  ref,
-  set
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-
-// Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCLCNqa083B7EVNnIQrazocXanW0Q43s7Y",
+  apiKey: "AIzaSyCLCNqa0B387EVNnIQrazoXanW0Q43s7Y",
   authDomain: "novacoretransit-7ee86.firebaseapp.com",
   databaseURL: "https://novacoretransit-7ee86-default-rtdb.firebaseio.com",
   projectId: "novacoretransit-7ee86",
   storageBucket: "novacoretransit-7ee86.appspot.com",
-  messagingSenderId: "649843732880",
-  appId: "1:649843732880:web:bc8eae809d1161070ea055",
+  messagingSenderId: "649834732880",
+  appId: "1:649834732880:web:bc8eae809d1161070ea055",
   measurementId: "G-27WD7PSVQW"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-// Registro de usuario
-const registerForm = document.getElementById("registerForm");
-if (registerForm) {
-  registerForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-      await set(ref(db, 'usuarios/' + user.uid), {
-        nombre: name,
-        correo: email,
-        saldo: 0.00
-      });
-      alert("✅ Registro exitoso");
-      registerForm.reset();
-      window.location.href = "login.html";
-    } catch (error) {
-      alert("❌ Error al registrar: " + error.message);
-    }
-  });
-}
+firebase.initializeApp(firebaseConfig);
